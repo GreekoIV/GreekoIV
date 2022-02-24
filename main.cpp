0ec9b65af8832e641e4131fa4da67777dabe7547
Greeko <97302388+GreekoIV@users.noreply.github.com>
2022-02-24 11:35:32 +0200

    Calculator Project

--- main.cpp
+++ main.cpp
@@ -1,36 +1,77 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
-int main()
-{
+class Calculator{
+    double num1, num2;
 
-    int secretNum = 8;
-    int guess;
-    int guessCount = 0;
-    int guessLimit = 4;
-    bool outOfGuesses = false;
-
-    while(secretNum != guess && !outOfGuesses){
-
-        if(guessCount < guessLimit){
-
-            cout << "Enter Guess: \n";
-            cin >> guess;
-            guessCount++;
-
-        }else{
-            outOfGuesses= true;
+public:
+        void get(){
+            cout<<"Enter First Number:\n";
+            cin>>num1;
+            cout<<"Enter Second Number:\n";
+            cin>>num2;
         }
 
-    }
-    if(outOfGuesses){
-        cout<<"You lose!\n";
-    }else{
-        cout<<"You won!\n";
-    }
+        double add(){return num1+num2;}
 
+        double mul(){return num1*num2;}
 
+        double sub(){
+            if(num2>num1){
+                return num2-num1;
+            }else{return num1-num2;}
+        }
+
+        double div(){
+            if(num2>num1){
+                cout<<"Division by Zero:"<<endl;
+                return INFINITY;
+            }else{return num1/num2;}
+        }
+};
+
+int main()
+{
+    int choice;
+    Calculator cal; //calculator object
+    cout << "Enter 1 Add 2 Numbers"
+        << "\n Enter 2 Subtract 2 Numbers"
+        << "\n Enter 3 Multiply 2 Numbers"
+        << "\n Enter 4 Divide 2 Numbers"
+        << "\n Enter 0 to Exit.."
+        << "\n";
+
+        do{
+            cout <<"\n Enter Choice : ";
+            cin >> choice ;
+
+            switch(choice){
+            case 1:
+                cal.get();
+                cout << "Result:" << cal.add() << endl;
+                break;
+
+            case 2:
+                cal.get();
+                cout << "Result:" << cal.sub() << endl;
+                break;
+
+            case 3:
+                cal.get();
+                cout << "Result:" << cal.mul() << endl;
+                break;
+
+            case 4:
+                cal.get();
+                cout << "Result:" << cal.div() <<endl;
+                break;
+
+            case 0:
+                break;
+            }
+        }while( choice >=1 && choice <= 4 );
 
     return 0;
 }
